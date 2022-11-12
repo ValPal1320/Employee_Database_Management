91413f8be44febbe18bfdb80023783b7d2ad47fe
Val Pal <valgonzalez13@yahoo.com>
2022-11-12 11:50:43 -0600

    list required dependencies

---
+++ index.js
@@ -0,0 +1,5 @@
+// Require list of dependencies
+const mysql2 = require('mysql2');
+const inquirer = require('inquirer');
+const consoleTable = require('console.table');
+const chalk = require('chalk');
