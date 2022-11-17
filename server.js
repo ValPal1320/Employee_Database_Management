6a5e0a3bcb033ab140b1244d1c6ee390396b7bfd
Val Pal <valgonzalez13@yahoo.com>
2022-11-17 00:06:22 -0600

    create mysql connection

--- server.js
+++ server.js
@@ -4,3 +4,16 @@ const inquirer = require("inquirer");
 const chalk = require("chalk");
 require("console.table");
 
+// Create mysql connection
+const connection = mysql2.createConnection({
+    host: 'localhost',
+    port: 3004,
+    user: 'root',
+    password: 'pickles512',
+    database: 'employee_db'
+});
+
+connection.connect(err => {
+    if (err) throw err;
+    firstPrompt();
+})
