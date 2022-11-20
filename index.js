207737f39dc11b44e02587632efdd7c57cb32de8
Val Pal <valgonzalez13@yahoo.com>
2022-11-19 21:52:05 -0600

    testing adding new employee

--- index.js
+++ index.js
@@ -94,8 +94,31 @@ function startPrompt() {
                     viewAllEmps();
                     break;
 
+                // Add a new employee
                 case promptMessages.addEmp:
-                    addEmp();
+                    inquirer
+                        .prompt([{
+                            type: 'input',
+                            name: 'firstName',
+                            message: "Employee's first name: "
+                        },
+                        {
+                            type: 'input',
+                            name: 'lastName',
+                            message: "Employee's last name: "
+                        },
+                        {
+                            type: 'input',
+                            name: 'role',
+                            message: 'Role ID: '
+                        },
+                        {
+                            type: 'input',
+                            name: 'managerId',
+                            message: "Manager's ID: "
+                        }]).then((answer) => {
+                            addEmp(answer.firstName, answer.lastName, answer.role, answer.managerId);
+                        })
                     break;
 
                 case promptMessages.updateEmpRole:
@@ -153,16 +176,30 @@ function addRole(roleName, salary, deptId) {
     });
 };
 
+// View all employees
 const viewAllEmps = async () => {
     console.log("All current employees:");
     const allEmps = await connection
         .promise()
         .query(
-            "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, manager_id AS manager ID FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id LEFT OUTER JOIN employee mgr ON employee.manager_id = mgr.id;"
+            `SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) FROM employee 
+            LEFT JOIN employee manager on manager.id = employee.manager_id
+            INNER JOIN role ON (role.id = employee.role_id)
+            INNER JOIN department ON (department.id = role.department_id)
+            ORDER BY employee.id`
         );
     console.log(allEmps[0]);
     startPrompt();
 };
+
+function addEmp(firstName, lastName, role, managerId) {
+    const mysql2 = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${firstName}", "${lastName}", "${role}", "${managerId}")`;
+    connection.query(mysql2, (err, rows) => {
+        if (err) throw err;
+        console.log("New employee successfully added to the table")
+        startPrompt();
+    });
+}
 
 
 
